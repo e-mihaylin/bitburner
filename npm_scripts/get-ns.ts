1d4863396d34502e16c860af99567d686a72b453
e-mihaylin <yevgeny.mihailin@gmail.com>
2025-04-18 13:58:48 +0300

    init, dev deps, ns typings fetch

---
+++ npm_scripts/get-ns.ts
@@ -0,0 +1,7 @@
+// @ts-ignore
+import wget from 'node-wget';
+
+ wget({
+   url: 'https://raw.github.com/bitburner-official/bitburner-src/dev/src/ScriptEditor/NetscriptDefinitions.d.ts',
+   dest: 'src/scripts/types/NetscriptDefinitions.d.ts',
+ });
