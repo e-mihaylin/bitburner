f9ce9a353305ce2588c910832123c087cb124305
e-mihaylin <yevgeny.mihailin@gmail.com>
2025-04-18 14:42:57 +0300

    remove bitburner-filesync, no ts support

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # bitburner
-bitburner game scripts
+[bitburner](https://store.steampowered.com/app/1812820/Bitburner/) user scripts
+
+scripts root directory 'src/scripts'
--- npm_scripts/get-ns.ts
+++ npm_scripts/get-ns.ts
@@ -3,5 +3,5 @@ import wget from 'node-wget';
 
  wget({
    url: 'https://raw.github.com/bitburner-official/bitburner-src/dev/src/ScriptEditor/NetscriptDefinitions.d.ts',
-   dest: 'src/scripts/types/NetscriptDefinitions.d.ts',
+   dest: 'src/scripts/types/ns.d.ts',
  });
